32725d4ef07cfc8a1694c00e0abb7c5696d32e3d
mxaba <samuel.mcebo@gmail.com>
2021-06-19 23:04:23 +0200

    i am ablew to greet a person

---
+++ greet.js
@@ -0,0 +1,74 @@
+module.exports = () => {
+  let languMessage = '';
+  let person = '';
+  let nameList = {};
+  let greetingsCounter = 0;
+
+  function langRun(la, per) {
+    if (la === 'spanish') {
+      languMessage = 'Hola, ';
+    } if (la === 'isizulu') {
+      languMessage = 'Sawubona, ';
+    } if (la === 'english') {
+      languMessage = 'Hello, ';
+    }
+    person = per;
+    // return languMessage + person
+  }
+
+  function checkErrors() {
+    if (person === '') {
+      return 'Please pass a name';
+    } if (languMessage === '' || languMessage === undefined) {
+      return 'Please Select a Language';
+    }
+    return '';
+  }
+
+  // Converting the first Letter
+  function capFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+
+  // Adding to the object
+  function nameLists(name) {
+    nameList = name;
+    if (nameList[person] === undefined) {
+      greetingsCounter += 1;
+      nameList[person] = 1;
+      return nameList;
+    }
+    nameList[person] += 1;
+    return nameList;
+  }
+
+  function greetPerson() {
+    return languMessage + person;
+  }
+
+  function getCounter() {
+    return greetingsCounter;
+  }
+
+  function getNameList() {
+    return nameList;
+  }
+
+  function getNAmeOnList(name) {
+    if (nameList[name] !== undefined) {
+      return false;
+    }
+    return true;
+  }
+
+  return {
+    getNAmeOnList,
+    greetPerson,
+    langRun,
+    checkErrors,
+    nameLists,
+    getCounter,
+    getNameList,
+    capFirstLetter,
+  };
+};
--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const expresshbs = require('express-handlebars');
 const bodyParse = require('body-parser');
+const expressFlash = require('express-flash');
+const greet = require('./greet');
 
 const app = express();
+const greetInsta = greet();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('view engine', 'handlebars');
 app.engine('handlebars', expresshbs({ defaultLayout: 'main' }));
+app.use(expressFlash());
 
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.post('/greetme', (req, res) => {
+  let { nameEntered } = req.body;
+  const { language } = req.body;
+
+  if (!language && nameEntered === '') {
+    req.flash('info', 'Please enter name and select language!');
+  } if (nameEntered === '') {
+    console.log('Please enter a name!');
+  } if (!language) {
+    console.log('Please select a language!');
+  } if (!/[a-zA-z]$/.test(nameEntered)) {
+    console.log('Please pass a valid name!');
+  } else {
+    nameEntered = greetInsta.capFirstLetter(nameEntered);
+    greetInsta.langRun(language, nameEntered);
+    greetInsta.nameLists(nameEntered);
+  }
+
+  res.render('home', {
+    greetPerson: greetInsta.greetPerson(),
+    greetings: greetInsta.getCounter(),
+  });
 });
 
 app.listen(PORT, () => {
